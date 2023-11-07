d310a63e33c3bd1147a30cb07dab05e0bc2fae93
tiztaw <tiztaw3@gmail.com>
2023-11-07 08:24:29 +0300

    add file

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import propTypes from 'prop-types'
-import './CourseList.css'
 
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
--- 0x04-React_inline_styling/task_3/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/CourseList/CourseListRow.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite';
 import propTypes from 'prop-types'
-import './CourseList.css'
 
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
@@ -12,25 +12,25 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
 	const header_row_background_color = { backgroundColor: '#deb5b545' };
 	let node;
 	let style;
-	
+
 	if (isHeader) {
 		style = header_row_background_color;
 		if (!textSecondCell) {
-			node = <th colSpan="2">{textFirstCell}</th>;
+			node = <th colSpan="2" className={css(rowStyles.th)}>{textFirstCell}</th>;
 		} else {
-			node = 
-		  <React.Fragment>
-			  <th>{textFirstCell}</th>
-			  <th>{textSecondCell}</th>
-		  </React.Fragment>;
+			node =
+				<React.Fragment>
+					<th className={css(rowStyles.NOTth)}>{textFirstCell}</th>
+					<th className={css(rowStyles.NOTth)}>{textSecondCell}</th>
+				</React.Fragment>;
 		}
 	} else {
 		style = row_background_color;
-		node = 
-		  <React.Fragment>
-			  <td>{textFirstCell}</td>
-			  <td>{textSecondCell}</td>
-		  </React.Fragment>;
+		node =
+			<React.Fragment>
+				<td>{textFirstCell}</td>
+				<td>{textSecondCell}</td>
+			</React.Fragment>;
 	}
 	return (
 		<tr style={style}>
@@ -38,6 +38,20 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
 		</tr>
 	);
 }
+
+const rowStyles = StyleSheet.create({
+	th: {
+		textAlign: 'center',
+		border: `1px solid`,
+		paddingBottom: `0.5rem`
+	},
+
+	NOTth: {
+		textAlign: 'start',
+		borderBottom: `1px solid`,
+	}
+})
+
 
 CourseListRow.defaultProps = {
 	isHeader: false,
